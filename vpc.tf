c80de5e03e8214474784dc90251e433be6778175
prakash <prakashecho@gmail.com>
2024-03-29 23:23:47 +0530

    8aa

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
     cidr = var.VpcCIDR
     azs = [var.Zone1,var.Zone2,var.Zone3]
     private_subnets = [var.PrivSub1CIDR,var.PrivSub2CIDR,var.PrivSub3CIDR]
-    public_subnets = [var.PubSub1CIDR,var.PrivSub2CIDR,var.PubSub3CIDR]
+    public_subnets = [var.PubSub1CIDR,var.PubSub2CIDR,var.PubSub3CIDR]
     enable_nat_gateway = true
     single_nat_gateway = true
     enable_dns_hostnames = true

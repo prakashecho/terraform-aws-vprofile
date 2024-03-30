890503afedf30ed64f7c97f94d32d91bf08e5be9
prakash <prakashecho@gmail.com>
2024-03-30 22:20:07 +0530

    sssssss

--- secgrp.tf
+++ secgrp.tf
@@ -70,21 +70,21 @@ resource "aws_security_group" "vprofile-backend-sg" {
         protocol = "-1"
         to_port = 0
         cidr_blocks = ["0.0.0.0/0"]
-    }
+        }
   
      ingress {
         from_port = 0
         protocol = "-1"
         to_port = 0
         security_groups = [aws_security_group.vprofile-prod-sg.id ]
-    }
+        }
 
      ingress = {
         from_port = 3306
         protocol = "tcp"
         to_port = 3306
         security_groups = [aws_security_group.vprofile-bastion-sg.id]
-    }
+         }
   
 }
 
